25d3d4523f17dd39b424599691c546069ae840be
si220 <saifullahijaz@hotmail.com>
2022-06-20 12:41:59 +0100

    Update back-end.js

--- Command/back-end.js
+++ Command/back-end.js
@@ -35,16 +35,37 @@ con.end((err) => {
     // Then sends a quit packet to the MySQL server.
 });  
     
-
+// Client's browser performs GET request to ask server to display HTML web page
 server.get('/', function(req, res) {
- res.sendFile('/Users/Owner/Documents/GitHub/2022Project_Rover/Command/index.html');
+    res.sendFile('/Users/Owner/Documents/GitHub/2022Project_Rover/Command/index.html');
 });
+
+// index.html requires styles.css so you have to add a GET request in node.js
 server.get('/styles.css', function(req, res) {
- res.sendFile('/Users/Owner/Documents/GitHub/2022Project_Rover/Command/styles.css');
+    res.sendFile('/Users/Owner/Documents/GitHub/2022Project_Rover/Command/styles.css');
 });
+
+// when a user clicks a button on the web app, data is sent to the server using a POST request
 server.post("/datastream", function (req, res) {
+    res.send(req.body.data);
+    //console.log(req.body.data);
+});
+
+server.get("/datastream", function (req, res) {
     res.send(req.body.data);
     console.log(req.body.data);
 });
+
+// testing 
+//console.log(movement);
+
+/* esp32 accesses the data using a GET request
+server.get("/datastream", function (req, res) {
+    res.send(req.body.data);
+    console.log('esp32 get request');
+    res.end('Hello');
+});
+*/
+
 console.log('Server is running on port 8000');
 server.listen(8000,'0.0.0.0');
